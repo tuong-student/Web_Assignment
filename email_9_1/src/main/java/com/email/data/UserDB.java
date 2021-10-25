333a98e1793fdf6b79a5534a6f5042626bba2f18
tuong-student <62384988+tuong-student@users.noreply.github.com>
2021-10-25 14:06:40 +0700

    add assignment 9.1

---
+++ email_9_1/src/main/java/com/email/data/UserDB.java
@@ -0,0 +1,12 @@
+package com.email.data;
+
+import com.email.business.User;
+
+public class UserDB {
+    
+    public static long insert(User user) {
+        // TODO: Add code that adds the user to the database
+        // NOTE: This is shown in chapters 11-13
+        return 0;
+    }
+}
